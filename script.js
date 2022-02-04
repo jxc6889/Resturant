de3d965f76a031946981a59ec722432ad09642a6
jxc6889 <jxc6889.rit.edu>
2022-02-04 03:11:17 -0500

    added menu

--- script.js
+++ script.js
@@ -13,21 +13,60 @@
             menuList.style.marginTop = "100px";
         }
     }
-    const menuheader = ["header1", "header2", "header3","header4", "header5", "header6" , "header7" , "header8" , "header9"];
+    const menuheader = ["Rice", "Noodles", "Fried Rice","Lunch Menu", "Appetizers", "Korean BBQ" , "Chef's Choice" , "Casserole" , "Grilled Fish", "Kid's Menu", "Soups and Stews"];
     function showmenu(menu){
         
         console.log(menu);
-        for (let i = 1; i < 10; i++) {
+        for (let i = 1; i < 12; i++) {
+          if(i < 10){
+            var item = document.getElementById("menu0"+i);
+            console.log("menu0"+i)
+            item.style.display = "none";
+          }
+          else{
             var item = document.getElementById("menu"+i);
             console.log("menu"+i)
             item.style.display = "none";
           }
-        
+            
+          }
+          var menu_nunber = menu.substring(menu.length - 2);
+          console.log(Number(menu_nunber));
         var show = document.getElementById(menu);
         show.style.display = "block";
-        document.getElementById("menuheader").innerHTML = menuheader[Number(menu.charAt(menu.length - 1))-1];
+        document.getElementById("menuheader").innerHTML = menuheader[Number(menu_nunber)-1];
         
-        console.log(menu.charAt(menu.length - 1));
+        
+        
+        if(menu != "menu02"){
+          document.getElementById("extra").style.display = "block";
+         
+          if(menu == "menu04"){
+           
+            document.getElementById("lunch-time").style.display = "block";
+          }
+          else{
+            document.getElementById("lunch-time").style.display = "none";
+          }
+         
+          if(menu == "menu06"){
+            
+            document.getElementById("kbbq").style.display = "block";
+          }
+          else{
+            document.getElementById("kbbq").style.display = "none";
+          }
+          if(menu == "menu10"){
+            document.getElementById("kid").style.display = "block";
+          }
+          else{
+            document.getElementById("kid").style.display = "none";
+          }
+        }
+        else{
+          document.getElementById("extra").style.display = "none";
+        
+        }
         
     }
     
@@ -57,7 +96,7 @@
     }
     window.addEventListener('scroll', () => {
       const scrolled = window.scrollY;
-      if(scrolled > 360){
+      if(scrolled > 400){
         document.getElementById("border").style.backgroundColor = "#262626";
 
       }
